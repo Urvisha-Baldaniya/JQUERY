d9e17e7dc6956d46bc84f0fc787cb880187ef790
Urvisha-Baldaniya <urvishabaldaniya12@gmail.com>
2024-11-11 20:02:43 +0530

    jquery

---
+++ Admin-Panel/js/charts.js
@@ -0,0 +1,211 @@
+ var options = {
+          series: [{
+          name: 'series1',
+          data: [31, 40, 28, 51, 42, 109, 100]
+        }, {
+          name: 'series2',
+          data: [11, 32, 45, 32, 34, 52, 41]
+        }],
+          chart: {
+          height: 350,
+          type: 'area'
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+        xaxis: {
+          type: 'datetime',
+          categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+        },
+        tooltip: {
+          x: {
+            format: 'dd/MM/yy HH:mm'
+          },
+        },
+        };
+
+        var chart = new ApexCharts(document.querySelector("#chart1"), options);
+        chart.render();
+
+
+
+  // MAP
+
+
+  $(document).ready(function(){
+    tippy('.tippy', {
+      theme: 'light',
+      size: 'big',
+      arrow: true
+    })
+});
+
+
+// CHART2
+  
+
+var options = {
+  series: [76, 67, 61, 90],
+  chart: {
+  height: 350,
+  type: 'radialBar',
+},
+plotOptions: {
+  radialBar: {
+    offsetY: 0,
+    startAngle: 0,
+    endAngle: 270,
+    hollow: {
+      margin: 5,
+      size: '30%',
+      background: 'transparent',
+      image: undefined,
+    },
+    dataLabels: {
+      name: {
+        show: false,
+      },
+      value: {
+        show: false,
+      }
+    }
+  }
+},
+colors: ['#1ab7ea', '#0084ff', '#39539E', '#0077B5'],
+labels: ['Vimeo', 'Messenger', 'Facebook', 'LinkedIn'],
+legend: {
+  show: true,
+  floating: true,
+  fontSize: '16px',
+  position: 'left',
+  offsetX: 160,
+  offsetY: 15,
+  labels: {
+    useSeriesColors: true,
+  },
+  markers: {
+    size: 0
+  },
+  formatter: function(seriesName, opts) {
+    return seriesName + ":  " + opts.w.globals.series[opts.seriesIndex]
+  },
+  itemMargin: {
+    vertical: 3
+  }
+},
+responsive: [{
+  breakpoint: 480,
+  options: {
+    legend: {
+        show: false
+    }
+  }
+}]
+};
+
+var chart = new ApexCharts(document.querySelector("#chart2"), options);
+chart.render();
+
+
+// CHART3
+
+
+var options = {
+  series: [{
+  name: 'Inflation',
+  data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2]
+}],
+  chart: {
+  height: 450,
+  type: 'bar',
+},
+plotOptions: {
+  bar: {
+    borderRadius: false,
+    dataLabels: {
+      position: 'top', // top, center, bottom
+    },
+  }
+},
+dataLabels: {
+  enabled: true,
+  formatter: function (val) {
+    return val + "%";
+  },
+  offsetY: -20,
+  style: {
+    fontSize: '12px',
+    colors: ["#304758"]
+  }
+},
+
+xaxis: {
+  categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+  position: 'bottom',
+  axisBorder: {
+    show: false
+  },
+  axisTicks: {
+    show: false
+  },
+  crosshairs: {
+    fill: {
+      type: 'gradient',
+      gradient: {
+        colorFrom: '#D8E3F0',
+        colorTo: '#BED1E6',
+        stops: [0, 100],
+        opacityFrom: 0.4,
+        opacityTo: 0.5,
+      }
+    }
+  },
+  tooltip: {
+    enabled: true,
+  }
+},
+yaxis: {
+  axisBorder: {
+    show: false
+  },
+  axisTicks: {
+    show: false,
+  },
+  labels: {
+    show: false,
+    formatter: function (val) {
+      return val + "%";
+    }
+  }
+
+},
+title: {
+  floating: true,
+  offsetY: 330,
+  align: 'center',
+  style: {
+    color: '#444'
+  }
+}
+};
+
+var chart = new ApexCharts(document.querySelector("#chart3"), options);
+chart.render();
+
+
+// HEADER
+
+$(".toggle").click(function(){
+  $("header,main,footer").toggleClass("toggle-click");
+  $("aside").toggleClass("aside-click");
+});
+
+$("header li a").click(function(){
+ $(this).parent("header li").find(".dropdown").slideToggle();
+ $(this).parent("header li").siblings("header li").find(".dropdown").css("display","none");
+});
+      
+      
